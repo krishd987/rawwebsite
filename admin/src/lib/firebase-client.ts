f7eed742b0c0423508682ded4d9b6d4e04f15c0b
taksh1507 <takshgandhi4@gmail.com>
2026-08-15 20:29:53 +0530

    Update environment variable names for Firebase Client SDK

--- admin/src/lib/firebase-client.ts
+++ admin/src/lib/firebase-client.ts
@@ -1,12 +1,12 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.CONFIG_FIREBASE_API_KEY,
+  authDomain: process.env.CONFIG_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.CONFIG_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.CONFIG_FIREBASE_APP_ID,
+  measurementId: process.env.CONFIG_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
