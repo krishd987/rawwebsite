679465630c2685e26a38c38e6afa33119e1c5086
taksh1507 <takshgandhi4@gmail.com>
2026-08-15 20:09:34 +0530

    changed the file details

--- admin/src/lib/firebase-client.ts
+++ admin/src/lib/firebase-client.ts
@@ -1,12 +1,12 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUJgV_mEj4i2bgZUuYxiY074OUA33F5EU",
-  authDomain: "rawwebsite-77710.firebaseapp.com",
-  projectId: "rawwebsite-77710",
-  messagingSenderId: "1046755775156",
-  appId: "1:1046755775156:web:779ab75c2d52d4a9313de1",
-  measurementId: "G-KZ1YPXCDCG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
